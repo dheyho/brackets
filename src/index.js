c446a2629b23b32e30267f759bec22b78046d011
dheyho <dheyho@mail.ru>
2021-03-12 23:13:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  str.split(" ");
+  let fuct1 = 0;
+  let fuct2 = 0;
+  let fuct3 = 0;
+  let fuct4 = 0;
+  let fuct5 = 0;
+  let fuct6 = 0;
+  let fuct7 = 0;
+  
+  for(let i = 0; i< str.length; i++){
+  if(str[i]=="["){
+  fuct1 ++;
+  }
+  
+  if(str[i]=="]"){
+  fuct2 ++;
+  }
+  
+  if(str[i]=="{"){
+  fuct3 ++;
+  }
+  
+  if(str[i]=="}"){
+  fuct4 ++;
+  }
+  
+  if(str[i]=="("){
+  fuct5 ++;
+  }
+  
+  if(str[i]==")"){
+  fuct6 ++;
+  }
+  
+  if(str[i]=="|"){
+  fuct7 ++;
+  }
+  }
+  
+  if((fuct1!=fuct2)||(fuct3!=fuct4)||(fuct5!=fuct6)){
+  return false;
+  }
+  
+  else if(fuct7 % 2 == 0){
+  return true;
+  }
+  else
+  return true;
+  }
